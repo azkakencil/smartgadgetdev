d0d7fcd9464b1dd3553116643543ee915f7db542
azkakencil <azkakencil6@gmail.com>
2026-05-27 07:29:12 +0700

    Fix formatting issue in firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,7 +16,7 @@ const FIREBASE_CONFIG = {
 ‎    projectId: "deploy2-ed9ce",
 ‎    storageBucket: "deploy2-ed9ce.firebasestorage.app",
 ‎    messagingSenderId: "405598518951",
-‎    appId: "1:405598518951:web:e20cc22b8aead19306e582"
+‎    appId: "1:405598518951:web:e20cc22b8aead19306e582",
   };
 let app: FirebaseApp;
 let db: ReturnType<typeof getDatabase>;
